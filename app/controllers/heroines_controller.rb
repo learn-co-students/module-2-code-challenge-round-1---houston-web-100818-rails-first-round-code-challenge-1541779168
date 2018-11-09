c3571dad51cbf710b37e66e3f44a566faf659c9c
Megan Farley <megfarley4@gmail.com>
2018-11-09 11:57:17 -0600

    done but no search

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,29 @@
 class HeroinesController < ApplicationController
+
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params(:name, :super_name, :power_id))
+    if @heroine.valid?
+      redirect_to @heroine
+    else
+      flash[:notice] = "Heroine must have a unique super name"
+      redirect_to '/heroines/new'
+    end
+  end
+
+  def heroine_params(*args)
+    params.require(:heroine).permit(*args)
+  end
+
 end
