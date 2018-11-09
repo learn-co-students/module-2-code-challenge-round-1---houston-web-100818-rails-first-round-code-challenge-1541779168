e33e638dbf223a9fe095ec7e7462c6d7474ad36d
barbaradecares <barbaradecares@hotmail.com>
2018-11-09 11:28:41 -0600

    done

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,12 @@
 class Heroine < ApplicationRecord
+    belongs_to :power
+    validates :super_name, uniqueness: true
+    
+    def self.search(params)
+        # byebug
+        power = Power.find_by(name: params)
+       if power 
+        where("power_id LIKE ?", "%#{power.id}%")
+       end
+    end 
 end
