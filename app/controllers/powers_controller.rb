b05d5350422ffa60b18b7451a5e2d8b50846e35f
github username <github email address>
2018-11-09 10:46:40 -0600

    Controllers done

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,46 @@
 class PowersController < ApplicationController
+
+  before_action :define_current_power
+
   def index
     @powers = Power.all
   end
+
+  def new
+    @power = Power.new
+  end
+    
+  def create
+    @power = Power.create(power_params)
+    redirect_to @power
+  end
+    
+  def show
+    @power = Power.find(params[:id])
+  end
+    
+  def edit
+    @power = Power.find(params[:id])
+  end
+    
+  def update
+    @power = Power.find(params[:id])
+    @power.update(power_params)
+    redirect_to @power
+  end
+
+  private
+
+  def define_current_power
+    if params[:id]
+        @power = Power.find(params[:id])
+    else
+        @power = Power.new
+    end
+  end
+
+  def power_params
+    params.require(:power).permit(:name, :description)
+  end
+
 end
